d8976e677278ac95dfb11e696d2149640867562c
Ananya V <ananyaav20@gmail.com>
2025-04-16 23:21:19 +0530

    Js First code(var let const)

---
+++ newFile.js
@@ -0,0 +1,20 @@
+var names="anu"
+let gender="female"
+var age=20
+const institute='x-workz'
+//document.writeln(age)
+
+let emp1={id:12,employeeName:'aa',salary:10000}
+let emp2=new Object();
+emp2.id=13;
+emp2.employeeName='anu'
+emp2.salary=60000
+emp2.id=14;
+
+function emp3(id, employeeName, salary){
+    this.id=id;
+    this.emloyee=employee;
+    this.salary=salary;
+}
+console.log(age)
+console.log(emp1)
