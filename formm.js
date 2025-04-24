a1a35a950a997e64fef6ac8d4ea37198b6acf330
Ananya V <ananyaav20@gmail.com>
2025-04-24 21:34:05 +0530

    form using js

---
+++ formm.js
@@ -0,0 +1,8 @@
+function submitForm(){
+        let name=document.formName.nameName.value
+        let num=document.formName.number.value
+        let email=document.formName.emailName.value
+        let Password=document.formName.password.value
+        let conPassword=document.formName.pass.value
+        alert(name+num+email)
+    }
